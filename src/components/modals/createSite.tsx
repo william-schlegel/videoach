63133cb82a6b1ee5b8af2b5fe4fab7c9c4e5bcc0
william-schlegel <william.schlegel@gmail.com>
2022-12-05 12:36:58 +0100

    create rooms

--- src/components/modals/createSite.tsx
+++ src/components/modals/createSite.tsx
@@ -6,7 +6,8 @@ import {
 } from "react-hook-form";
 import Modal from "../ui/modal";
 import SimpleForm from "../ui/simpleform";
-import { CgAdd } from "react-icons/cg";
+import { CgAdd, CgTrash } from "react-icons/cg";
+import { useState } from "react";
 
 type FormValues = {
   name: string;
@@ -24,6 +25,7 @@ const CreateSite = ({ clubId }: CreateSiteProps) => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>();
+  const [rooms, setRooms] = useState<string[] | null>(null);
 
   const createSite = trpc.sites.createSite.useMutation({
     onSuccess: () => {
@@ -38,6 +40,27 @@ const CreateSite = ({ clubId }: CreateSiteProps) => {
 
   const onError: SubmitErrorHandler<FormValues> = (errors) => {
     console.log("errors", errors);
+  };
+
+  const handleChangeRoom = (idx: number, name: string) => {
+    const rs = rooms ? [...rooms] : [""];
+    rs[idx] = name;
+    setRooms(rs);
+  };
+
+  const addRoom = () => {
+    const rs = rooms;
+    if (!rs) setRooms([""]);
+    else {
+      rs.push("");
+      setRooms([...rs]);
+    }
+  };
+
+  const deleteRoom = (idx: number) => {
+    if (!rooms) return;
+    const rs = rooms.filter((_, i) => i !== idx);
+    setRooms(rs.length ? [...rs] : null);
   };
 
   return (
@@ -69,6 +92,23 @@ const CreateSite = ({ clubId }: CreateSiteProps) => {
           },
         ]}
       />
+      <div className="divider"></div>
+      <div className="flex items-center gap-4">
+        <h3>Salles</h3>
+        <button className="btn-outline btn-secondary btn" onClick={addRoom}>
+          <CgAdd size={24} />
+          Ajouter
+        </button>
+      </div>
+      {rooms?.map((room, idx) => (
+        <div key={idx} className="my-2 flex items-center gap-2">
+          <input
+            value={room}
+            onChange={(e) => handleChangeRoom(idx, e.target.value)}
+          />
+          <CgTrash size={24} color="red" onClick={() => deleteRoom(idx)} />
+        </div>
+      ))}
     </Modal>
   );
 };
