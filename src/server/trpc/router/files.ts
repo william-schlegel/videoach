ba44f7a489f9ae6fff2c0755bac0ba1f4a35c3c0
william-schlegel <william.schlegel@gmail.com>
2023-01-06 15:10:05 +0100

    change aws env

--- src/server/trpc/router/files.ts
+++ src/server/trpc/router/files.ts
@@ -14,8 +14,8 @@ import { env } from "@root/src/env/server.mjs";
 const s3 = new S3Client({
   region: "eu-west-3",
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY_ID ?? "",
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY ?? "",
+    accessKeyId: env.AWS_ACCESS_KEY_ID_WSC ?? "",
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY_WSC ?? "",
   },
 });
 
